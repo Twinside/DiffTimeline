f9af9f6d7f3a9e0c0c082bb2da2c24f3f22f7ba0
Vincent Berthoux <twinside@gmail.com>
2011-10-07 19:34:04 +0200

    Diff algorithm implementation

--- lib/diff.rb
+++ lib/diff.rb
@@ -1,3 +1,5 @@
+require 'pp'
+
 module GitRead
     class Diff
         def initialize( orig, dest )
@@ -6,50 +8,95 @@ module GitRead
             @dest = dest
         end
 
-    private
-        def compute_diff
-            @coeffs.generate do |line, col|
-                if line == 0 || col == 0
-                    return 0
-                end
+        def dump
+            @coeffs.dump
+        end
 
-                if @orig[i] == @dest[j]
-                    return @coeffs[i - 1][j - 1] + 1
+        def Diff.diffFiles( filename1, filename2 )
+            f1 = open(filename1, 'rb') do |file|
+                file.read().lines.to_a
+            end
+
+            f2 = open(filename2, 'rb') do |file|
+                file.read().lines.to_a
+            end
+
+            return nil if f1 == nil || f2 == nil
+            diff = Diff.new( f1, f2 )
+            diff.compute_diff
+            diff
+        end
+
+        def compute_diff
+            puts "Mkay"
+            @coeffs.generate do |i, j|
+                l = @orig[i - 1]
+                r = @dest[j - 1]
+                if l == r
+                    @coeffs[i - 1][j - 1] + 1
                 else
                     left = @coeffs[i - 1][j]
                     down = @coeffs[i][j - 1]
-                    return left if left > down
-                    return down
+                    if left > down
+                        left
+                    else
+                        down
+                    end
                 end
+            end
+        end
+
+        def print_diff
+            print_diff_at(@orig.length, @dest.length)
+        end
+
+        def print_diff_at(i, j)
+            if i > 0 && j > 0 && @orig[i - 1] == @dest[j - 1]
+
+                print_diff_at(i - 1, j - 1)
+                print ("(    )  " + @orig[i - 1])
+
+            elsif j > 0 && (i == 0 || @coeffs[i][j-1] >= @coeffs[i-1][j])
+
+                print_diff_at(i, j-1)
+                print (("(%4d)+ " % i) + @dest[j - 1])
+
+            elsif i > 0 && (j == 0 || @coeffs[i][j-1] < @coeffs[i-1][j])
+
+                print_diff_at(i - 1, j)
+                print (("(%4d)- " % i) + @orig[i - 1])
+
+            else
+                puts ""
             end
         end
 
         class CoeffTable
             def initialize(length1, length2)
-                @data = Array.new(length1 + 1) { Array.new(length2 + 1) }
+                @data = Array.new(length1 + 1) { Array.new(length2 + 1, 0) }
                 @orig_length = length1
                 @dest_length = length2
-
-                # set the first line to 0
-                @data[0][0..length2] = 0
-                # set the first column to 0
-                @data[0..length 1][0] = 0
             end
 
             def generate
-                for line in (0 .. @orig_length - 1)
-                    for column in (0 .. @dest_length - 1)
+                puts @orig_length
+                puts @dest_length
+                for line in 1 .. @orig_length
+                    for column in 1 .. @dest_length
                         @data[line][column] = yield line,column
                     end
                 end
             end
 
-            def [](x, y)
-                @data[x][y]
+            def [](x)
+                @data[x]
             end
 
-            def []=(x, y, value)
-                @data[x][y] = value
+            def dump
+                @data.each do |line|
+                    line.each { |v| print "%4d " % v }
+                    print "\n"
+                end
             end
         end
     end
---
+++ test/diff_test.rb
@@ -0,0 +1,6 @@
+require_relative '../lib/diff.rb'
+
+diff = GitRead::Diff.diffFiles('test/file1_orig.txt', 'test/file1_dest.txt')
+diff.dump
+puts "\n=====================================\n"
+diff.print_diff
