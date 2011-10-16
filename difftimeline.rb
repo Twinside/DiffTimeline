fb4f835b5c5614bd846383298ce97f396621b847
Vincent Berthoux <twinside@gmail.com>
2011-10-16 16:22:18 +0200

    Strengthening server against errors

--- difftimeline.rb
+++ difftimeline.rb
@@ -19,10 +19,23 @@ end
 
 exec_path = Pathname(__FILE__).realpath.parent
 
+class RepositoryNotFound < RuntimeError 
+end
+
+class FileNotInRepository < RuntimeError
+    attr_reader :filename
+    def initialize(f)
+        @filename = f
+    end
+end
+
 class DiffTimelineState
     def initialize
         @current_dir = Pathname.new(Dir.pwd)
         repo_dir = find_nearest_git_repo()
+
+        raise RepositoryNotFound if repo_dir.nil?
+
         @tracked_path = (@current_dir + Pathname.new(ARGV[0])).cleanpath.relative_path_from(Pathname.new(repo_dir))
         @repository = GitRead::Repository.new(repo_dir)
         @current_head = @repository.head_sha
@@ -37,9 +50,22 @@ class DiffTimelineState
         end
     end
 
+    def send_error_message(what)
+        response = { "error" => what }
+
+        [200, {'Content-Type' => 'text/json'}, [response.to_json]]
+    end
+
     def load_parent(query_string)
         pp "Asking #{query_string}"
         commit = @repository.access_object(GitRead::ShaRef.new(query_string.to_s))
+
+        if commit.nil?
+            return send_error_message("Commit not found #{query_string}")
+        elsif commit.class != GitRead::Commit
+            return send_error_message("#{query_string} is not a commit.")
+        end
+
         file = commit.tree.access_path(@tracked_path.to_s)
         diff = GitRead::Diff.diff_strings(file.data, @last_file.data)
         encoded = { 
@@ -53,10 +79,34 @@ class DiffTimelineState
         [200, {'Content-Type' => 'text/json'}, [response]]
     end
 
+    def serve_not_found_error
+        html_error = <<END
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+        <title>#{@tracked_path}</title>
+        <link href="difftimeline.css" type="text/css" rel="stylesheet" />
+    </head>
+    <body onUnload="leave_server()">
+        <div class="error_panel">
+            File not found in repository : #{@tracked_path}
+        </div>
+    </body>
+</html>
+END
+        [200, {'Content-Type' => 'text/html'}, [html_error]]
+    end
+
     def serve_base_page
         commit = @repository.access_object(@current_head)
         tree = commit.tree
         file = tree.access_path(@tracked_path.to_s)
+
+        if file.nil?
+            return serve_not_found_error()
+        end
+
         encoded_data = html_encode_file(file.data)
         @last_file = file
 
@@ -87,6 +137,7 @@ class DiffTimelineState
                 </div>
             </div>
         </div>
+        <div class="message_carret"></div>
     </body>
 </html>
 END
@@ -94,7 +145,13 @@ END
     end
 end
 
-state = DiffTimelineState.new()
+state = nil
+begin
+    state = DiffTimelineState.new()
+rescue RepositoryNotFound 
+    puts "Error : no git repository found"
+    exit(1)
+end
 
 # Serious race condition, but it will be ok to test
 Launchy.open('http://127.0.0.1:8080')
@@ -131,7 +188,11 @@ Net::HTTP::Server.run(:host => '127.0.0.1', :port => 8080) do |request,socket|
   elsif requested == '/quit'
       puts "Leaving"
       exit 0
+
+  # Security problem on the next line, permit access to an atacker to any file
+  # on the machine.
   elsif File.exists?('.' + requested) && requested != '/'
+      pp requested
       serve_file(exec_path + requested.to_s.slice(1, requested.size))
   else
       state.serve_base_page
