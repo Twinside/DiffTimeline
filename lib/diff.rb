1f48e1a6b0e7eaeeedfa1ed99bb56f3c1758eaab
Vincent Berthoux <twinside@gmail.com>
2011-10-25 18:09:57 +0200

    Implemented LCS algorithm (the one from the diff command), instead of the naïve one, good speedup

--- lib/diff.rb
+++ lib/diff.rb
@@ -1,22 +1,23 @@
-require 'pp'
 
 module GitRead
     class Diff
+        attr_reader :diff_set
+
         # Class describin a diff action to pass from
         # origin to destination.
         class DiffCommand
             attr_reader :cmd, :orig_idx, :dest_idx, :size
 
             # (:add_line | :rem_line, Int, Int) -> DiffCommand
-            def initialize(cmd, idx, ddx)
+            def initialize(cmd, idx, ddx, size)
                 @cmd = cmd
-                @size = 1
+                @size = size
                 @orig_idx = idx
                 @dest_idx = ddx
             end
 
-            def inc_size
-                @size += 1
+            def inc_size(s)
+                @size += s
             end
 
             def to_json(*a)
@@ -32,9 +33,29 @@ module GitRead
             end
         end
 
+        def print_diff
+            @diff_set.each do |d|
+                case d.cmd
+                when :rem_line
+                    (d.orig_idx .. d.orig_idx + d.size - 1).each do |i| 
+                        puts "- (#{i}) #{@orig[i]}"
+                    end
+
+                when :add_line
+                    (d.dest_idx .. d.dest_idx + d.size - 1).each do |i|
+                        puts "+ (#{i}) #{@dest[i]}"
+                    end
+                end
+            end
+        end
+
         class DiffSet
             def initialize()
                 @listing = []
+            end
+
+            def each
+                @listing.each { |v| yield v }
             end
 
             def to_json(*a)
@@ -42,31 +63,31 @@ module GitRead
                 '[ ' + json + ' ]'
             end
 
-            def add_line(orig_line, dest_line)
+            def add_range(orig_line, dest_line, size)
                 if @listing.size == 0
-                    @listing << DiffCommand.new(:add_line, orig_line, dest_line)
+                    @listing << DiffCommand.new(:add_line, orig_line, dest_line, size)
                 else
                     prev = @listing.last
 
                     if prev.cmd == :add_line && dest_line == prev.dest_idx + prev.size
-                        @listing.last.inc_size
+                        @listing.last.inc_size(size)
                     else
-                        @listing << DiffCommand.new(:add_line, orig_line, dest_line)
+                        @listing << DiffCommand.new(:add_line, orig_line, dest_line, size)
                     end
                 end
             end
 
-            def rem_line(orig_line, dest_line)
+            def rem_range(orig_line, dest_line, size)
                 if @listing.size == 0
-                    @listing << DiffCommand.new(:rem_line, orig_line, dest_line)
+                    @listing << DiffCommand.new(:rem_line, orig_line, dest_line, size)
                     return
                 else
                     prev = @listing.last
 
                     if prev.cmd == :rem_line && orig_line == prev.orig_idx + prev.size
-                        @listing.last.inc_size
+                        @listing.last.inc_size(size)
                     else
-                        @listing << DiffCommand.new(:rem_line, orig_line, dest_line)
+                        @listing << DiffCommand.new(:rem_line, orig_line, dest_line, size)
                     end
                 end
             end
@@ -78,47 +99,160 @@ module GitRead
             @orig = orig
             @dest = dest
 
-            calculate_identical_offsets()
+            @orig_hash = Array.new(@orig.size) { |i| @orig[i].hash }
+            @dest_hash = Array.new(@dest.size) { |i| @dest[i].hash }
 
-            @coeffs = Array.new(@orig_effective_size + 1) { Array.new(@dest_effective_size + 1, 0) }
+            coef_size = (@orig.size + @dest.size) * 2
+            @forward_snake = Array.new(coef_size, -1)
+            @backward_snake = Array.new(coef_size, -1)
+
+            @diff_set = DiffSet.new
+            compute_diff
         end
 
-        # We try to reduce the diff to perform by finding
-        # the identical beginning & end
-        def calculate_identical_offsets
-            i = 0
-            while i < @orig.size && @dest.size
-                if @orig[i] != @dest[i]
-                    break
-                end
-                i += 1
+        def compute_diff
+            lcs(0, @orig_hash.size, 0, @dest_hash.size)
+        end
+
+        def lcs(orig_begin, orig_end, dest_begin, dest_end)
+
+            while orig_begin < orig_end && dest_begin < dest_end && @orig_hash[orig_begin] == @dest_hash[dest_begin]
+                orig_begin += 1
+                dest_begin += 1
             end
-            @begin_offset = i
 
-            orig_end = @orig.size - 1
-            dest_end = @dest.size - 1
-
-            while orig_end > 0 && dest_end > 0 && orig_end >= @begin_offset
-                if @orig[orig_end] != @dest[dest_end]
-                    break
-                end
+            while orig_begin < orig_end && dest_begin < dest_end && @orig_hash[orig_end - 1] == @dest_hash[dest_end - 1]
                 orig_end -= 1
                 dest_end -= 1
             end
 
-            @orig_effective_size = orig_end - @begin_offset + 1
-            @dest_effective_size = dest_end - @begin_offset + 1
+            # we found a snake
+            if orig_begin == orig_end
+                @diff_set.add_range(orig_begin, dest_begin, dest_end - dest_begin)
+                return
+            elsif dest_begin == dest_end
+                @diff_set.rem_range(orig_begin, dest_begin, orig_end - orig_begin)
+                return
+            else
+                snake_range = find_middle_snake(orig_begin, orig_end, dest_begin, dest_end)
 
-            if @orig_effective_size > 0
-                @orig_hash = Array.new(@orig_effective_size) { |i| @orig[i + @begin_offset].hash }
-                @dest_hash = Array.new(@dest_effective_size) { |i| @dest[i + @begin_offset].hash }
+                lcs(orig_begin, snake_range.x, dest_begin, snake_range.y)
+                lcs(snake_range.x, orig_end, snake_range.y, dest_end)
             end
         end
 
-        # nil
-        # Print the coefficient table on stdout
-        def dump
-            @coeffs.dump
+        def find_max_reaching_snake(x,y, max_x, max_y)
+            while x < max_x && y < max_y && @orig_hash[x] == @dest_hash[y]
+                x += 1
+                y += 1
+            end
+
+            x
+        end
+
+        def find_max_reaching_snake_backward(x,y, x_min, y_min)
+            while x_min < x && y_min < y && @orig_hash[x - 1] == @dest_hash[y - 1]
+                x -= 1
+                y -= 1
+            end
+
+            x
+        end
+
+        class DiffPoint
+            attr_accessor :x, :y
+
+            def initialize(x,y)
+                @x = x
+                @y = y
+            end
+        end
+
+        class Range
+            attr_accessor :min, :max
+            def initialize(mini, maxi)
+                @min = mini
+                @max = maxi
+            end
+
+            # (Range, Vector, Int) -> ()
+            def increase_within(bounds, vector, null_value)
+                if @min > bounds.min
+                    @min -= 1
+                    vector[@min - 1] = null_value
+                else
+                    @min += 1
+                end
+
+                if @max < bounds.max
+                    @max += 1
+                    vector[ @max + 1 ] = null_value
+                else
+                    @max -= 1
+                end
+            end
+
+            def range_backward
+                d = @max
+                while d >= @min
+                    yield d
+                    d -= 2
+                end
+            end
+
+            def contain(x)
+                @min <= x && x <= @max
+            end
+        end
+
+        def find_middle_snake(x_min, x_max, y_min, y_max)
+            valid_diagonal = Range.new(x_min - y_max, x_max - y_min)
+
+            forward_mid = x_min - y_min
+            backward_mid = x_max - y_max
+
+            forward_range = Range.new(forward_mid, forward_mid)
+            backward_range = Range.new(backward_mid, backward_mid)
+
+            @forward_snake[forward_mid] = x_min
+            @backward_snake[backward_mid] = x_max
+
+            is_odd = (forward_mid - backward_mid).odd?
+
+            while true
+                forward_range.increase_within(valid_diagonal, @forward_snake, -1)
+                forward_range.range_backward do |d|
+                    lower_diag = @forward_snake[d - 1]
+                    upper_diag = @forward_snake[d + 1]
+                    # we choose the biggest diagonal, the one reaching the
+                    # furthest
+                    x = upper_diag
+                    x = lower_diag + 1 if lower_diag >= upper_diag
+
+                    snake = find_max_reaching_snake(x, x - d, x_max, y_max)
+                    @forward_snake[d] = snake
+
+                    if is_odd && backward_range.contain(d) && @backward_snake[d] <= snake
+                        return DiffPoint.new(x, x - d)
+                    end
+                end
+
+                backward_range.increase_within(valid_diagonal, @backward_snake, 9999999)
+                backward_range.range_backward do |d|
+                    lower_diag = @backward_snake[d - 1]
+                    # we choose the smallest diagonal, the one reaching the furthest.
+                    upper_diag = @backward_snake[d + 1]
+                    x = lower_diag
+                    x = upper_diag - 1 if lower_diag >= upper_diag
+
+                    snake = find_max_reaching_snake_backward(x, x - d, x_min, y_min)
+                    @backward_snake[d] = snake
+
+                    if !is_odd && forward_range.contain(d) && snake <= @forward_snake[d]
+                        return DiffPoint.new(x, x - d)
+                    end
+                end
+            end
         end
 
         # (Filepath, Filepath) -> Diff
@@ -134,141 +268,15 @@ module GitRead
             end
 
             return nil if f1 == nil || f2 == nil
-            diff = Diff.new( f1, f2 )
-            diff.compute_diff
-            diff
+            Diff.new( f1, f2 )
         end
 
         # (String, String) -> Diff
         def Diff.diff_strings( file_content_a, file_content_b )
             lines_a = file_content_a.lines.to_a
             lines_b = file_content_b.lines.to_a
-            diff = Diff.new( lines_a, lines_b )
-            diff.compute_diff
-            diff
-        end
-
-        # nil
-        # Compute the coefficient matrix.
-        def compute_diff
-            for i in 1 .. @orig_effective_size
-                for j in 1 .. @dest_effective_size
-
-                    full_i = i + @begin_offset
-                    full_j = j + @begin_offset
-
-                    l = @orig_hash[i - 1]
-                    r = @dest_hash[j - 1]
-                    # Identical, so the matching score is
-                    # the matching score of previous string +1
-                    if l == r
-                        @coeffs[i][j] = @coeffs[i - 1][j - 1] + 1
-                    else
-                        #          ^
-                        #          | down
-                        #          |
-                        # left +---+--+
-                        #   <--+      |
-                        #      +------+
-
-                        # correspond to a score with a deletion
-                        # of the origin
-                        down = @coeffs[i - 1][j] 
-
-                        # correspond to a score with an addition
-                        # to the origin
-                        left = @coeffs[i][j - 1]
-
-                        # otherwise, it's the maximum score
-                        # of the previously calculated data.
-                        @coeffs[i][j] = [left, down].max
-                    end
-                end
-            end
-        end
-
-        # DiffSet
-        def diff_set
-            if @orig_effective_size <= 0
-                return DiffSet.new
-            end
-
-            acc = DiffSet.new
-            diff_set_at(@orig_effective_size, @dest_effective_size, acc)
-            acc
-        end
-
-        # (Int, Int, [DiffCommand]) -> nil
-        def diff_set_at(i,j, rez)
-            real_i = @begin_offset + i
-            real_j = @begin_offset + j
-            l = @orig_hash[i - 1]
-            r = @dest_hash[j - 1]
-
-            # We're not at the edge of the matrix, and data
-            # is identical, so we're matching, no diff information
-            # here.
-            if i > 0 && j > 0 && l == r
-                diff_set_at(i - 1, j - 1, rez)
-            # i == 0 => no more lines in origin, so must add everything
-            # else.
-            # coeffs i,(j-1) >= coeffs (i-1),j =>
-            # if we delete a line from the destination we get a better
-            # matching score (in comparison of deleting a line from origin),
-            # so declare an addition for the diff
-            elsif j > 0 && (i == 0 || @coeffs[i][j-1] >= @coeffs[i-1][j])
-                diff_set_at(i, j - 1, rez)
-                rez.add_line( real_i - 1, real_j - 1)
-            # inversly to previous case we get a better score by removing
-            # a line, so follow this diff
-            elsif i > 0 && (j == 0 || @coeffs[i][j-1] < @coeffs[i-1][j])
-                diff_set_at(i - 1, j, rez)
-                rez.rem_line(real_i - 1, real_j - 1)
-            end
-        end
-
-        # nil
-        # print the diff on stdout.
-        def print_diff(only_diff)
-            if !only_diff
-                (0..(@begin_offset - 1)).each { |v| print "(BEGI)  #{@orig[v]}" }
-            end
-
-            print_diff_at(@orig_effective_size, @dest_effective_size, only_diff)
-
-            if !only_diff
-                ((@begin_offset + @orig_effective_size)..(@orig.size - 1)).each do |v|
-                    puts "(END )  #{@orig[v]}"
-                end
-            end
-        end
-
-        # print the diff on stdout.
-        # (Int, Int) -> nil
-        def print_diff_at(i, j, only_diff)
-            real_i = @begin_offset + i
-            real_j = @begin_offset + j
-            l = @orig[real_i - 1]
-            r = @dest[real_j - 1]
-
-            if i > 0 && j > 0 && l == r
-
-                print_diff_at(i - 1, j - 1, only_diff)
-                print ("(    )  " + l) if !only_diff
-
-            elsif j > 0 && (i == 0 || @coeffs[i][j-1] >= @coeffs[i-1][j])
-
-                print_diff_at(i, j-1, only_diff)
-                print (("(%4d)+ " % real_i) + r)
-
-            elsif i > 0 && (j == 0 || @coeffs[i][j-1] < @coeffs[i-1][j])
-
-                print_diff_at(i - 1, j, only_diff)
-                print (("(%4d)- " % real_i) + l)
-
-            else
-                puts ""
-            end
+            Diff.new( lines_a, lines_b )
         end
     end
 end
+
--- test/diff_test.rb
+++ test/diff_test.rb
@@ -12,25 +12,37 @@ require_relative '../lib/diff.rb'
 
 diff = GitRead::Diff.diff_files('test/file1_orig.txt', 'test/file1_dest.txt')
 puts "\n=====================================\n"
-diff.print_diff(false)
+diff.print_diff
 puts diff.diff_set.to_json
 
+# ignoring this part for now
 (0..2).each do |v|
     puts "\n=====================================\n"
     puts "Diffing different in middle"
     beg_time = Time.now
     diff = GitRead::Diff.diff_files('test/huge1.cpp', 'test/huge2.cpp')
     puts "#{Time.now - beg_time}"
-    diff.print_diff(true)
+    diff.print_diff
     puts diff.diff_set.to_json
 
+    #diff test/huge1.cpp test/huge2.cpp 
+    #879,880d878
+    #<   this->height = size;
+    #<   this->width  = size;
+    #1183a1182
+    #>   /* blah blah */
     puts "\n=====================================\n"
 
     puts "Diffing different both ends"
     beg_time = Time.now
     diff = GitRead::Diff.diff_files('test/huge1.cpp', 'test/huge3.cpp')
     puts "#{Time.now - beg_time}"
+    #diff test/huge1.cpp test/huge3.cpp
+    #1d0
+    #< #include "TextureUtils.h"
+    #2336a2336
+    #> /* PWET */
 
-    diff.print_diff(true)
+    diff.print_diff
     puts diff.diff_set.to_json
 end
