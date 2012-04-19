e33ced7f280ef31182e981075cb81f040ed80226
Vincent Berthoux <twinside@gmail.com>
2012-04-19 23:33:04 +0200

    Adding browser autolaunch

--- Difftimeline/main.hs
+++ Difftimeline/main.hs
@@ -1,8 +1,28 @@
-import Prelude              (IO)
-import Yesod.Default.Config (fromArgs)
-import Yesod.Default.Main   (defaultMain)
-import Settings             (parseExtra)
+import Prelude              (IO, ($), Maybe(..) )
+import Yesod.Default.Config (--fromArgs, loadDevelopmentConfig
+                             AppConfig(..), DefaultEnv( Development ))
+-- import Yesod.Default.Main   (defaultMain)
+import Settings             (Extra(..))
 import Application          (getApplication)
+import Network.Wai.Handler.Launch (run)
+-- import Network.Wai.Application.Static
+import Yesod.Logger ( -- Logger, logBS, logString,
+                     defaultDevelopmentLogger)
+-- import Network.Wai.Middleware.RequestLogger (logCallbackDev)
 
 main :: IO ()
-main = defaultMain (fromArgs parseExtra) getApplication
+main = do
+    let config = AppConfig {
+        appEnv = Development,
+        appPort = 3000,
+        appRoot = ".",
+        appExtra = Extra {
+                extraCopyright = "",
+                extraAnalytics = Nothing
+            }
+      }
+    -- config <- fromArgs parseExtra
+    logger <- defaultDevelopmentLogger
+    app <- getApplication config logger
+    run app
+
