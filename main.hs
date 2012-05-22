cc4dc2efb275f8df4db6228bb7a8a748eb29fd56
Vincent Berthoux <twinside@gmail.com>
2012-05-22 10:17:36 +0200

    Handling multiple instances

--- main.hs
+++ main.hs
@@ -1,28 +1,58 @@
 import Prelude
-import Yesod.Default.Config (AppConfig(..), DefaultEnv( Development ))
+import Yesod.Default.Config( AppConfig(..)
+                           , DefaultEnv( Development )
+                           )
+
 import Difftimeline.Application( getApplication )
-import Network.Wai.Handler.Launch (run)
+import Network.Wai.Handler.Launch( runUrlPort )
 import System.Environment( getArgs )
-import Yesod.Logger (defaultDevelopmentLogger)
+import Yesod.Logger( defaultDevelopmentLogger )
+import Network.Socket( socket
+                     , Family( AF_INET )
+                     , SocketType( Datagram )
+                     , defaultProtocol
+                     , aNY_PORT
+                     , sClose
+                     , bindSocket
+                     , SockAddr( SockAddrInet )
+                     , iNADDR_ANY
+                     , PortNumber( PortNum )
+                     , socketPort
+                     )
+
+-- | Ugly workaround to find a free port :
+-- create a socket with any port as configuration and retrieve
+-- it.
+-- Not safe at all, as after that, a race condition can happen
+-- in the system, leaving a wrong socket down the flow
+findNextPort :: IO Int
+findNextPort = do
+    s <- socket AF_INET Datagram defaultProtocol
+    bindSocket s (SockAddrInet aNY_PORT iNADDR_ANY)
+    PortNum i <- socketPort s
+    sClose s
+    return $ fromIntegral i
 
 main :: IO ()
 main = do
-    let config = AppConfig {
-        appEnv = Development,
-        appPort = 3000,
-        appRoot = ".",
-        appHost = "",
-        appExtra = ()
-      }
     args <- getArgs
+    usePort <- findNextPort
+    let runner = runUrlPort usePort ""
+        config = AppConfig {
+            appEnv = Development,
+            appPort = usePort,
+            appRoot = ".",
+            appHost = "",
+            appExtra = ()
+      }
     case args of
         [] -> do
             logger <- defaultDevelopmentLogger
             app <- getApplication Nothing config logger
-            run app
+            runner app
 
         (f:_) -> do
             logger <- defaultDevelopmentLogger
             app <- getApplication (Just f) config logger
-            run app
+            runner app
 
