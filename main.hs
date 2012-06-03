b15e113ff96907069ea8e7339a0733ff29ba12f8
Vincent Berthoux <twinside@gmail.com>
2012-06-03 22:43:55 +0200

    Adding command line option to force server listening port

--- main.hs
+++ main.hs
@@ -1,13 +1,11 @@
 import Prelude
-import Data.Maybe( listToMaybe )
-import Yesod.Default.Config( AppConfig(..)
-                           , DefaultEnv( Development )
-                           )
 
+import Control.Monad( when )
+import Data.List( foldl' )
+import Data.Maybe( listToMaybe )
 import Difftimeline.Application( getApplication )
+
 import Network.Wai.Handler.Launch( runUrlPort )
-import System.Environment( getArgs )
-import Yesod.Logger( defaultDevelopmentLogger )
 import Network.Socket( socket
                      , Family( AF_INET )
                      , SocketType( Datagram )
@@ -20,6 +18,59 @@ import Network.Socket( socket
                      , PortNumber( PortNum )
                      , socketPort
                      )
+
+import System.Environment( getArgs )
+import System.Exit( exitWith, ExitCode( ExitSuccess ) )
+import System.Console.GetOpt( OptDescr( Option )
+                            , ArgDescr( ReqArg, NoArg )
+                            , ArgOrder( Permute )
+                            , getOpt
+                            , usageInfo
+                            )
+
+import Yesod.Default.Config( AppConfig(..)
+                           , DefaultEnv( Development )
+                           )
+
+import Yesod.Logger( defaultDevelopmentLogger )
+
+version :: String
+version = "1.0"
+
+-- | Type representing flag present on the command line
+data Flag = Port String
+          | Help
+          deriving Eq
+
+-- | Command line configuration
+data Conf = Conf
+    { confShowHelp :: !Bool
+    , confPort     :: Maybe Int
+    }
+
+-- | Initial and default configuration
+defaultConf :: Conf
+defaultConf = Conf
+    { confShowHelp = False
+    , confPort     = Nothing
+    }
+
+-- | Command line description
+options :: [OptDescr Flag]
+options =
+    [ Option "p" ["port"] (ReqArg Port "number")
+                "Server port number (random by default)"
+    , Option "" ["help"] (NoArg Help) "Show help (this screen)"
+    ]
+
+parseArgs :: IO (Conf, [String])
+parseArgs = do
+    args <- getArgs
+    let (opt, rest, _) = getOpt Permute options args
+    return (foldl' configurator defaultConf opt, rest)
+
+     where configurator c Help = c{ confShowHelp = True }
+           configurator c (Port n) = c { confPort = Just $ read n }
 
 -- | Ugly workaround to find a free port :
 -- create a socket with any port as configuration and retrieve
@@ -36,8 +87,16 @@ findNextPort = do
 
 main :: IO ()
 main = do
-    args <- getArgs
-    usePort <- findNextPort
+    (conf, args) <- parseArgs
+
+    when (confShowHelp conf)
+         (do putStrLn $ usageInfo ("Difftimeline v" ++ version) options
+             exitWith ExitSuccess)
+
+    usePort <- case confPort conf of
+        Nothing -> findNextPort
+        Just p -> return p
+
     let config = AppConfig {
             appEnv = Development,
             appPort = usePort,
@@ -45,6 +104,7 @@ main = do
             appHost = "",
             appExtra = ()
       }
+
     logger <- defaultDevelopmentLogger
     app <- getApplication (listToMaybe args) config logger
     runUrlPort usePort "" app
