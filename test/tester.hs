e3b0d53fb0491ad92a41d0dd1f3d260a598f028e
Vincent Berthoux <twinside@gmail.com>
2012-07-03 22:51:32 +0200

    Just a multiline commit to test the GUI second line and a very veeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeryyyyyyyyyyyyyyy looooooooooooooooooooooooooooooooooooooooooong lineeeeeeeeeeeeeeeeeeeeeeeeeeeeee to test overflooooooooooooooooooow

--- test/tester.hs
+++ test/tester.hs
@@ -9,6 +9,8 @@ import qualified Data.ByteString.Lazy as BL
 import Data.Aeson( ToJSON(..), encode )
 import Data.Git
 
+import Difftimeline.Diff
+
 serializeTest :: ToJSON a => a -> IO ()
 serializeTest =
     putStrLn . T.unpack . E.decodeUtf8 . B.concat . BL.toChunks . encode . toJSON
@@ -21,12 +23,19 @@ commitTreeDiffs =
 
 main :: IO ()
 main = do
-    putStrLn $ show nullRef
-    serializeTest $ ErrorReturn "anError"
-    serializeTest commitTreeDiff
-    serializeTest commitTreeDiffs
+    {-putStrLn $ show nullRef-}
+    {-serializeTest $ ErrorReturn "anError"-}
+    {-serializeTest commitTreeDiff-}
+    {-serializeTest commitTreeDiffs-}
+    {-putStrLn . show $-}
+               {-computeTextScript 3 (T.pack "Hello: Hello\nab")-}
+                                   {-(T.pack "Hello: Hello\nab")-}
+
+    {-putStrLn . show $-}
+               {-computeTextScript 3 (T.pack "Hello: Hello\n")-}
+                                   {-(T.pack "Hello: Hello\r\n")-}
 
     repo <- openRepo "./.git"
     Just headRef <- getHead repo
-    diffRez <- diffCommit repo 3 True $ fromHexString "dcc2bb741bf7d2ccde50517aeb3473e909cbff4c" -- headRef 
+    diffRez <- workingDirectoryChanges repo 3 headRef 
     putStrLn $ show diffRez
