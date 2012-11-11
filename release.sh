99061b94c4072a198c879be6363a9b4602b8805a
Vincent Berthoux <twinside@gmail.com>
2012-11-11 19:41:12 +0100

    Adding a script to generate release automatically

---
+++ release.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# Configuration
+OS=`uname`
+VERSION="1.0b1"
+
+# First little cleanup
+rm -Rf cabal-dev
+rm -Rf dist
+
+# Then make sure we are up to date
+git pull
+git submodule update
+
+cd hit-simple
+rm *.gz
+make pack
+cd ..
+
+cd ClosureExternalProducer
+rm *.gz
+make pack
+cd ..
+
+# make sure cabal-dev is here
+export PATH=$HOME/.cabal/bin:$PATH
+
+# append files
+cabal-dev add-source hit-simple/hit-simple-0.3.tar.gz
+cabal-dev add-source ClosureExternalProducer/ClosureExternalProducer-0.1.tar.gz
+cabal-dev install
+
+# Prepare
+cp dist/build/difftimeline/difftimeline .
+strip difftimeline
+tar cvjf difftimeline-$VERSION-$OS.tar.bz2 difftimeline
+
