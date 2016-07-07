bb5be21facd0109e8441aa34f56daf101f73998f
Vincent Berthoux <twinside@gmail.com>
2016-07-07 22:44:10 +0200

    Avoid loading blob when diffing commits...

--- Difftimeline/CommitDiff.hs
+++ Difftimeline/CommitDiff.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE TupleSections #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 module Difftimeline.CommitDiff ( createCommitDiff ) where
 
 import Prelude
@@ -6,6 +7,8 @@ import Prelude
 import Data.List( sortBy )
 import Data.Byteable( toBytes )
 import Control.Monad.Trans.Except( ExceptT, throwE, catchE )
+import Control.Monad.Trans.Reader( ReaderT, runReaderT, ask, asks )
+import Control.Monad.Trans.Class( lift )
 import Control.Monad.IO.Class( liftIO )
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Char8 as BC
@@ -27,6 +30,39 @@ import Difftimeline.Diff
 import Difftimeline.BaseLayer
 import Difftimeline.Contract
 
+data DiffContext = DiffContext
+  { _diffRepository :: !Git
+  , _diffContextSize :: !Int
+  , _diffDeep :: !Bool
+  }
+
+type ExceptIO = ExceptT String IO
+
+type DiffMonad a = ReaderT DiffContext ExceptIO a
+
+catchAppend :: ExceptIO a -> ExceptIO ([a] -> [a])
+catchAppend m = ((:) <$> m) `catchE` (\_ -> return id)
+
+catchAppendL :: forall a. DiffMonad a -> DiffMonad ([a] -> [a])
+catchAppendL v = do
+  b <- ask
+  lift $ catchAppend $ runReaderT v b
+
+maySubTree :: (T.Text -> Ref -> CommitTreeDiff) -> String -> Ref
+           -> DiffMonad CommitTreeDiff
+maySubTree f name r = do
+  repository <- asks _diffRepository
+  sub <- liftIO $ accessObject repository r
+  -- | Sometimes, there is nothing to see
+  case sub of
+     Nothing -> return $ f (T.pack name) r
+     Just el -> lift $ batchSubTree repository f name r el
+
+getCommit :: String -> Ref -> DiffMonad Object
+getCommit msg ref = do
+  repo <- asks _diffRepository
+  lift $ accessCommit msg repo ref
+
 -- | Compare two commits
 createCommitDiff :: Git
                  -> Int  -- ^ Context size to embed
@@ -34,76 +70,84 @@ createCommitDiff :: Git
                  -> Ref  -- ^ Base commit
                  -> Ref  -- ^ Final commit (destination)
                  -> ExceptT String IO (Commit, CommitTreeDiff)
-createCommitDiff repository contextSize deep prevRef ref = do
-    ObjCommit thisCommit <- accessCommit "Error can't file commit" repository ref
+createCommitDiff repository contextSize deep prevRef ref = runReaderT go base where
+  base = DiffContext repository contextSize deep
+
+  go = do
+    ObjCommit thisCommit <- getCommit "Error can't file commit" ref
     thisTree <- getObj "Error can't access commit tree" $ commitTreeish thisCommit
+    (prevTreeRef, prevTree) <-
+      if prevRef /= nullRef then do
+        ObjCommit prevCommit <- getCommit  "Error can't file parent commit" prevRef
+        let prevTreeRef = commitTreeish prevCommit
+        (prevTreeRef,) <$> getObj "Error can't access previous commit tree" prevTreeRef
+      else
+        return (nullRef, ObjTree (Tree []))
+    (thisCommit,) <$> diffObjects "" prevTree thisTree (commitTreeish thisCommit)
+ 
+    
 
-    (prevTreeRef, prevTree) <- if prevRef /= nullRef
-            then do ObjCommit prevCommit <- accessCommit "Error can't file parent commit" repository prevRef
-                    let prevTreeRef = commitTreeish prevCommit
-                    (prevTreeRef,) <$> getObj "Error can't access previous commit tree" prevTreeRef
-            else return (nullRef, ObjTree (Tree []))
+getObj :: String -> Ref -> DiffMonad Object
+getObj reason ref = do
+  repo <- asks _diffRepository
+  lift $ errorIO reason $ accessObject repo ref
 
-    (,) thisCommit <$> inner "" prevTree prevTreeRef thisTree (commitTreeish thisCommit)
-  where getObj reason = errorIO reason . accessObject repository
-        inner name (ObjTree (Tree left)) _r1 (ObjTree (Tree right)) _r2 = 
-          TreeElement (T.pack name) nullRef <$> diffTree name sortedLeft sortedRight
-            where sortedLeft = sortBy (\(_,a,_) (_,b,_) -> compare a b) left
-                  sortedRight = sortBy (\(_,a,_) (_,b,_) -> compare a b) right
+diffFileContent :: Bool -> Int -> FilePath -> Ref -> L.ByteString -> L.ByteString
+                -> CommitTreeDiff
+diffFileContent deep _ name ref c1 c2
+  | not deep = ModifyElement (T.pack name) ref []
+  | detectBinary c2 = ModifyBinaryElement (T.pack name) ref
+diffFileContent deep contextSize name ref c1 c2 =
+  ModifyElement (T.pack name) ref $! computeTextScript contextSize txtLeft txtRight
+    where 
+      strictify = B.concat . L.toChunks
+      txtLeft = decodeUtf8 $ strictify c1
+      txtRight = decodeUtf8 $ strictify c2
 
-        inner name (ObjBlob (Blob c1)) r1 (ObjBlob (Blob c2)) r2
-            | r1 == r2  = return $ NeutralElement (T.pack name) r1
-            | not deep  = return $ ModifyElement (T.pack name) r2 []
-            | detectBinary c2  = return $ ModifyBinaryElement (T.pack name) r2
-            | otherwise = return .
-                ModifyElement (T.pack name) r2 $! computeTextScript contextSize txtLeft txtRight
-                    where strictify = B.concat . L.toChunks
-                          txtLeft = decodeUtf8 $ strictify c1
-                          txtRight = decodeUtf8 $ strictify c2
-        inner _ _ _ _ _ = throwE "Wrong git object kind"
+diffObjects :: FilePath -> Object -> Object -> Ref -> DiffMonad CommitTreeDiff
+diffObjects name (ObjTree (Tree left)) (ObjTree (Tree right)) _r2 = 
+  TreeElement (T.pack name) nullRef <$> diffTree name sortedLeft sortedRight
+    where sortedLeft = sortBy (\(_,a,_) (_,b,_) -> compare a b) left
+          sortedRight = sortBy (\(_,a,_) (_,b,_) -> compare a b) right
+diffObjects name (ObjBlob (Blob c1)) (ObjBlob (Blob c2)) r = do
+  deep <- asks _diffDeep
+  context <- asks _diffContextSize
+  return $ diffFileContent deep context name r c1 c2
+diffObjects _ _ _ _= lift $ throwE "Wrong git object kind"
 
-        maySubTree :: (T.Text -> Ref -> CommitTreeDiff) -> String -> Ref
-                   -> ExceptT String IO CommitTreeDiff
-        maySubTree f name r = do
-            sub <- liftIO $ accessObject repository r
-            -- | Sometimes, there is nothing to see
-            case sub of
-               Nothing -> return $ f (T.pack name) r
-               Just el  -> batchSubTree repository f name r el
+diffTree :: String -> [TreeEnt] -> [TreeEnt]
+         -> DiffMonad [CommitTreeDiff]
+diffTree _name   []     [] = return []
+diffTree _name    [] rights = sequence
+    [maySubTree AddElement fullName r | (_, item, r) <- rights
+                                      , let fullName = BC.unpack $ toBytes item]
+diffTree _name lefts     [] = sequence
+    [maySubTree DelElement fullName r | (_, item, r) <- lefts
+                                      , let fullName = BC.unpack $ toBytes item ]
+diffTree name lefts@((_, lName, lRef):ls) rights@((_, rName, rRef):rs)
+    | lName == rName && lRef == rRef = 
+       (NeutralElement (decodeUtf8 $ toBytes lName) lRef:) <$> diffTree name ls rs
+    | lName == rName = do
+        repo <- asks _diffRepository
+        maySubL <- liftIO $ accessObject repo lRef
+        maySubR <- liftIO $ accessObject repo rRef
+        case (maySubL, maySubR) of
+          -- This case should happen in presence of submodules.
+          (Nothing, Nothing) -> (thisElem :) <$> diffTree name ls rs
+             where thisElem = ModifyElement (decodeUtf8 $ toBytes lName) lRef []
+          (Just subL, Just subR) -> do
+              diffInfo <- catchAppendL $ diffObjects (BC.unpack $ toBytes lName) subL subR rRef
+              diffInfo <$> diffTree name ls rs
+          (Nothing, _) ->
+              lift . throwE $ "Cannot fetch parent sub tree (" ++ toHexString lRef ++ ")"
 
-        diffTree :: String -> [TreeEnt] -> [TreeEnt]
-                 -> ExceptT String IO [CommitTreeDiff]
-        diffTree _name   []     [] = return []
-        diffTree _name    [] rights = sequence
-            [maySubTree AddElement fullName r | (_, item, r) <- rights
-                                              , let fullName = BC.unpack $ toBytes item]
-        diffTree _name lefts     [] = sequence
-            [maySubTree DelElement fullName r | (_, item, r) <- lefts
-                                              , let fullName = BC.unpack $ toBytes item ]
-        diffTree name lefts@((_, lName, lRef):ls) rights@((_, rName, rRef):rs)
-            | lName == rName = do
-                maySubL <- liftIO $ accessObject repository lRef
-                maySubR <- liftIO $ accessObject repository rRef
+          (_, Nothing) ->
+              lift . throwE $ "Cannot fetch this sub tree (" ++ toHexString rRef ++ ")"
 
-                case (maySubL, maySubR) of
-                  -- This case should happen in presence of submodules.
-                  (Nothing, Nothing) -> (thisElem :) <$> diffTree name ls rs
-                        where thisElem | rRef == rRef = NeutralElement (decodeUtf8 $ toBytes lName) lRef
-                                       | otherwise = ModifyElement (decodeUtf8 $ toBytes lName) lRef []
+    | lName < rName =
+        (:) <$> maySubTree DelElement (BC.unpack $ toBytes lName) lRef
+            <*> diffTree name ls rights
 
-                  (Just subL, Just subR) ->
-                        (((:) <$> inner (BC.unpack $ toBytes lName) subL lRef subR rRef)
-                                `catchE` (\_ -> return id)) <*> diffTree name ls rs
-                  (Nothing, _) ->
-                      throwE $ "Cannot fetch parent sub tree (" ++ toHexString lRef ++ ")"
-
-                  (_, Nothing) ->
-                      throwE $ "Cannot fetch this sub tree (" ++ toHexString rRef ++ ")"
-
-            | lName < rName =
-                (:) <$> maySubTree DelElement (BC.unpack $ toBytes lName) lRef
-                    <*> diffTree name ls rights
-
-            | otherwise = (:) <$> maySubTree AddElement (BC.unpack $ toBytes rName) rRef
-                              <*> diffTree name lefts rs
+    | otherwise = (:) <$> maySubTree AddElement (BC.unpack $ toBytes rName) rRef
+                      <*> diffTree name lefts rs
 
